2586d82a439e76b8fe2fb1ba414f1ad6acedcbd2
Shadwoo <looktouhid@gmail.com>
2025-10-21 19:44:26 +0530

    Update __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -1,5 +1,3 @@
-# FallenRobot/__init__.py
-
 import logging
 import os
 import sys
@@ -83,6 +81,9 @@ else:
 ALLOW_EXCL = bool(os.environ.get("ALLOW_EXCL", True))
 LOAD = os.environ.get("LOAD", "").split()
 NO_LOAD = os.environ.get("NO_LOAD", "").split()
+DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
+STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+WORKERS = int(os.environ.get("WORKERS", 8))
 
 # ---------- Core user sets ----------
 
@@ -110,6 +111,44 @@ DEV_USERS = list(DEV_USERS)
 DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 WOLVES = list(WOLVES)
+
+# ---------- Load modules ----------
+
+def __list_all_modules():
+    import glob
+    from os.path import basename, dirname, isfile
+
+    # This generates a list of modules in this folder for the * in __main__ to work.
+    mod_paths = glob.glob(dirname(__file__) + "/modules/*.py")
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+
+    if LOAD or NO_LOAD:
+        to_load = LOAD
+        if to_load:
+            if not all(
+                any(mod == module_name for module_name in all_modules)
+                for mod in to_load
+            ):
+                LOGGER.error("Invalid loadorder names. Quitting.")
+                quit(1)
+        else:
+            to_load = all_modules
+
+        if NO_LOAD:
+            LOGGER.info("Modules to load: %s", to_load)
+            return [item for item in to_load if item not in NO_LOAD]
+
+        return to_load
+
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+LOGGER.info("Modules to load: %s", ALL_MODULES)
 
 # ---------- Custom handler patch ----------
 

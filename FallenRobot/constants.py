b995e0ab72e03b464231cf3ab6c24d44dc972d8a
Shadwoo <looktouhid@gmail.com>
2025-10-21 19:35:55 +0530

    Delete FallenRobot/constants.py

--- FallenRobot/constants.py
+++
@@ -1,6 +0,0 @@
-# FallenRobot/constants.py
-import os
-
-ALLOW_EXCL = bool(os.environ.get("ALLOW_EXCL", True))
-LOAD = os.environ.get("LOAD", "").split()
-NO_LOAD = os.environ.get("NO_LOAD", "").split()

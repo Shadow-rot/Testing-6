1c508b261b483a4926f6b6e2d15b08f1e7885066
Shadwoo <looktouhid@gmail.com>
2025-10-21 16:10:07 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -18,7 +18,7 @@ class Config(object):
 
     SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "7891572866:AAEKgMqTNK0vQ_mAw63YFKdL6bD2oEiss14"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 

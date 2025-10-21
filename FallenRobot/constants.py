9dcc22ee517935a68bffff0522b5ac73e7d6754b
Shadwoo <looktouhid@gmail.com>
2025-10-21 17:15:38 +0530

    Update constants.py

--- FallenRobot/constants.py
+++ FallenRobot/constants.py
@@ -1,6 +1,6 @@
 # FallenRobot/constants.py
 import os
 
-ALLOW_EXCL = bool(os.environ.get("ALLOW_EXCL", False))
+ALLOW_EXCL = bool(os.environ.get("ALLOW_EXCL", True))
 LOAD = os.environ.get("LOAD", "").split()
 NO_LOAD = os.environ.get("NO_LOAD", "").split()
